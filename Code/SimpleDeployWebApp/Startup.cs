a2b22c5301023e311a5520c08d4960f6f01c4623
Sean Killeen <SeanKilleen@gmail.com>
2016-02-16 16:06:32 -0500

    Remove auth from sites

--- Code/SimpleDeployWebApp/Startup.cs
+++ Code/SimpleDeployWebApp/Startup.cs
@@ -8,7 +8,7 @@ namespace SimpleDeployWebApp
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            //ConfigureAuth(app);
         }
     }
 }
